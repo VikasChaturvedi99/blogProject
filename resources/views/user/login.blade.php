fed575936bf7405b9a8f2f0e567a36f034c73c9a
VikasChaturvedi99 <81855964+VikasChaturvedi99@users.noreply.github.com>
2024-07-21 09:04:06 +0530

    Initial commit

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>user Login</title>
+</head>
+<body>
+    <form method="POST" action="{{ route('user.login') }}">
+        @csrf
+        <div>
+            <label>Email</label>
+            <input type="email" name="email" required>
+        </div>
+        <div>
+            <label>Password</label>
+            <input type="password" name="password" required>
+        </div>
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
+</html>
