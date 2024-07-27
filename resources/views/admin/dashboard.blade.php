717eccc31c0f596bfe5d121cc8d060e3f74f2a55
VikasChaturvedi99 <81855964+VikasChaturvedi99@users.noreply.github.com>
2024-07-27 19:55:54 +0530

    commint

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,9 @@
-admin Dasbode
+
+@extends('admin.layout.app')
+
+@section('title', 'Home Page')
+
+@section('content')
+    <h2>Welcome to the Home Page</h2>
+    <p>This is the main content of the home page.</p>
+@endsection
