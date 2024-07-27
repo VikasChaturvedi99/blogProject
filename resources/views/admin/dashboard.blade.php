52091049123c4703f6d8a0de9918528e619826a0
VikasChaturvedi99 <81855964+VikasChaturvedi99@users.noreply.github.com>
2024-07-27 20:52:45 +0530

    update delete all

--- resources/views/admin/dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-
-@extends('admin.layout.app')
-
-@section('title', 'Home Page')
-
-@section('content')
-    <h2>Welcome to the Home Page</h2>
-    <p>This is the main content of the home page.</p>
-@endsection
